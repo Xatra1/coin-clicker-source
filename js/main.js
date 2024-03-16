8899b01ed177d1aaa68499830c7ff417ad3ce8d1
Xatra1 <124910045+Xatra1@users.noreply.github.com>
2024-03-16 03:32:47 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1906,6 +1906,7 @@ document.addEventListener('loadevt', function () {
             setTimeout(function () {
               if (!gameStarted) sfx7.play();
               if (!prompting) $('#updatestring').css('-webkit-animation-play-state', 'running');
+              updateString.textContent = navigator.userAgent;
               setTimeout(function () {
                 $('#title').css('-webkit-animation-play-state', 'paused');
                 smallCoin3.style.rotate = '270deg';
